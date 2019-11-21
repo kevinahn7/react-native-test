7a856712a7db38dee9b577fd2b5503c26380c763
Kevin Ahn <hyah@microsoft.com>
2019-11-21 15:32:17 -0800

    Add more features

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -41,6 +41,7 @@ const StartGameScreen = props => {
     setConfirmed(true);
     setSelectedNumber(chosenNumber);
     setEnteredValue('');
+    Keyboard.dismiss();
   };
 
   let confirmedOutput;
@@ -50,7 +51,7 @@ const StartGameScreen = props => {
       <Card style={styles.summaryContainer}>
         <Text>You selected</Text>
         <NumberContainer>{selectedNumber}</NumberContainer>
-        <Button title="START GAME"/>
+        <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)}/>
       </Card>
     )
   }
@@ -120,7 +121,7 @@ const styles = StyleSheet.create({
   },
   summaryContainer: {
     marginTop: 20,
-    alignItems: 'center'
+    alignItems: 'center',
   }
 });
 
